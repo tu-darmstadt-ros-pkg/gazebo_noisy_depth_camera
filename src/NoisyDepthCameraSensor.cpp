10d3e75bbac30ea07905fc3597b130b781e7a7bd
Martin Oehler <oehler@sim.tu-darmstadt.de>
2024-08-06 13:50:11 +0200

    Removed unused include

--- src/NoisyDepthCameraSensor.cpp
+++ src/NoisyDepthCameraSensor.cpp
@@ -8,7 +8,6 @@
 #include <gazebo/rendering/Scene.hh>
 #include <gazebo/rendering/RenderingIface.hh>
 #include <gazebo/rendering/RenderEngine.hh>
-#include <gazebo/rendering/Scene.hh>
 
 #include <functional>
 
